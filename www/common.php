0a8c5fd0328a28b88eb90822ecc45f81489df0c0
ura2404 <ura@itx.ru>
2020-08-14 20:07:12 +0300

    .

--- www/common.php
+++ www/common.php
@@ -3,15 +3,16 @@ require_once dirname(__FILE__) .'/module/Cmatrix/code/utils.php';
 
 spl_autoload_register(function($className){
     $isDm = 'cmDatamodel' === substr($className,0,11) ? true : false;
+    $className = $isDm ? substr($className,11) : $className;
     
     $ClassPath = str_replace("\\",DIRECTORY_SEPARATOR,$className).'.class.php';
-    dump($ClassPath,'ClassPath');
+    //dump($ClassPath,'ClassPath');
     $Root = dirname(__FILE__).'/module';
     
     array_map(function($value) use($isDm,$Root,$ClassPath){
         if($value == '.' || $value == '..' || !is_dir($Root .'/'. $value)) return;
         $Path = $Root.'/'.$value .($isDm ? '/dm/' : '/code/'). $ClassPath;
-        //dump($Path);
+        dump($Path);
         if(file_exists($Path)) {
             dump($Path,'require'.($isDm?1:0));
             require_once $Path;
