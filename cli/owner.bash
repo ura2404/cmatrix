06f1aaadabd7bb763c8c24c381d7873978ea50b5
ura2404 <ura@itx.ru>
2020-12-31 08:33:37 +0300

    .

--- cli/owner.bash
+++ cli/owner.bash
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-#FOLDER=`pwd | grep -o [^/]* | sed -e '$!{h;d;}' -e x`
-#echo $FOLDER
-
 HOME=`pwd`
-cd ..
+FOLDER=`pwd | grep -o [^/]* | sed -e '$!{h;d;}' -e x`
 
-chmod 770 ..
+cd ../..
+
+chmod 770 $FOLDER
+
+cd $FOLDER
 
 echo -e 'Mode for executable files.'
 find cli -type f \( -name "*.php" -or -name "*.sh" -or -name "*.bash" \) -exec chmod 770 {} \;
 
-#echo -e 'Mode for files.'
-#find * -type f -not -path 'cli/*' -exec chmod 660 {} \;
+echo -e 'Mode for files.'
+find * -type f -not -path 'cli/*' -exec chmod 660 {} \;
 
-#echo -e 'Mode for folders.'
-#find * -type d -exec chmod 770 {} \;
+echo -e 'Mode for folders.'
+find * -type d -exec chmod 770 {} \;
 
 echo
 cd $HOME
