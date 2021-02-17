bbd861de01067218607e6518e14b733e3389eb46
Ura2404 <ura@itx.ru>
2021-02-17 20:22:10 +0300

    .

--- cli/owner.bash
+++ cli/owner.bash
@@ -8,15 +8,15 @@ cd ../..
 chmod 770 $FOLDER
 
 echo -e 'Mode for executable files.'
-find $FOLDER -type f \( -path "$FOLDER/cli/*" -and -not -path '*/\.*' -and \( -name "*.php" -or -name "*.sh" -or -name "*.bash" \) \) -exec chmod 770 {} \;
+find $FOLDER -type f \( -path "$FOLDER/cli/*" -and -not -path '*/\.ht*' -and \( -name "*.php" -or -name "*.sh" -or -name "*.bash" \) \) -exec chmod 770 {} \;
 
 echo -e 'Mode for folders.'
 #find $FOLDER -type d -exec chmod 770 {} \; 2>/dev/null
-find $FOLDER -type d -not -path '*/\.*' -exec chmod 770 {} \;
+find $FOLDER -type d -not -path '*/\.ht*' -exec chmod 770 {} \;
 
 echo -e 'Mode for files.'
 #find $FOLDER -type f -not -path "$FOLDER/cli/*" -exec chmod 660 {} \; 2>/dev/null
-find $FOLDER -type f \( -not -path "$FOLDER/cli/*" -and -not -path '*/\.*' \) -exec chmod 660 {} \;
+find $FOLDER -type f \( -not -path "$FOLDER/cli/*" -and -not -path '*/\.ht*' \) -exec chmod 660 {} \;
 
 echo
 cd $HOME
